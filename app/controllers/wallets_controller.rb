7ead8b4962c0230c67aa329dee0f8379818dc48c
KayKawa <goodenoughliving1221@gmail.com>
2021-09-23 05:07:05 +0900

    TailwindCSSで仮のVIEWを作成

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,10 +1,11 @@
 class WalletsController < ApplicationController
   def index
     @wallets = Wallet.all
-    @users = User.all
   end
 
   def show
     @wallet = Wallet.find(params[:id])
   end
 end
+
+private
